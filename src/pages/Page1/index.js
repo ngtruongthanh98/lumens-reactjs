7fb247c3161fa401eb3b845ef3601baa75f36ffd
thanhnguyen98 <ngtruongthanh98@gmail.com>
2022-03-17 15:28:09 +0700

    Page header

--- src/pages/Page1/index.js
+++ src/pages/Page1/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import BackButton from '../../components/BackButton';
+import './styles.scss';
 
 const Page1 = () => {
   return (
-    <div>
-      <BackButton />
+    <div className="page-1">
+      <div className="header">
+        <BackButton />
+
+        <div className="content-box">
+          <div className="title">Silver Tier</div>
+          <div className="content">
+            In Silver Tier, every $1 in rental fee paid, you get 2 coins to redeem exclusive
+            rewards.
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
